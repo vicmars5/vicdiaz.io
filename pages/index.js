35dd85ee0bc3a95ba5a0043856d3a3002d71acca
Victor Agustín Diaz Mendez <mars.vic5@gmail.com>
2020-07-11 14:53:53 -0500

    show page title

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ pageTitle, description, ...props}) {
   return (
-    <div className="container">
+    <div className="container mx-auto mt-5">
       <Head>
-        <title>Create Next App</title>
+        <title>{pageTitle}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" description={description} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <main>
-        <h1>Hello world!</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      pageTitle: 'Vic\'s Blog',
+      description: 'Hablo de lo que me gusta: Ingenieria de software, programación y desarrollo web.'
+    }
+  }
 }
