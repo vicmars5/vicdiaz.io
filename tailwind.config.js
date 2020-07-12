6294fcb37ab7d86b4e92f73c66112219213e5d81
Victor Agustín Diaz Mendez <mars.vic5@gmail.com>
2020-07-12 15:05:47 -0500

    improved page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    screens: {
+      tablet: '640px',
+      laptop: '760px',
+      desktop: '760px'
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
