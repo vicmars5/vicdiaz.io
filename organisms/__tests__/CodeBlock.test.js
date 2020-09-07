d4d00c7aa68ee47d879e2f156e8cecddaad24e9b
Victor Diaz <vicdiaz@Victors-MacBook-Pro.local>
2020-09-07 16:54:11 -0500

    jest config

---
+++ organisms/__tests__/CodeBlock.test.js
@@ -0,0 +1,10 @@
+import CodeBlock from '../CodeBlock'
+import { shallow } from 'enzyme'
+
+describe('CodeBlock', () => {
+  it('should render', () => {
+    const wrapper = shallow(
+      <CodeBlock value='console.log()' language='js' />
+    )
+  })
+})
