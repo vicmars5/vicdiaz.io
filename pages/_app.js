b444e3403242fa156f583617f1814b04527fb337
Victor Diaz <mars.vic5@gmail.com>
2020-09-10 16:08:39 -0500

    resolved linter warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 // pages/_app.js
+import PropTypes from 'prop-types'
 import '../styles/index.css'
 
 export default function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.node,
+  pageProps: PropTypes.object
 }
